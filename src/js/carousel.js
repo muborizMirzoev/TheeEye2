c8ff5086b4febd1cfabb6b39d6a69c4fc25aa9eb
muborizMirzoev <muboriz10@gmail.com>
2021-09-03 03:16:46 +0700

    feat: add features locations block

---
+++ src/js/carousel.js
@@ -0,0 +1,27 @@
+const carousel = document.querySelector('#carousel')
+
+
+/* конфигурация */
+let width = 269+18; // ширина картинки
+let count = 4; // видимое количество изображений
+
+let list = carousel.querySelector('.carousel__images');
+let listElems = carousel.querySelectorAll('.carousel__item');
+
+let position = 0; // положение ленты прокрутки
+
+carousel.querySelector('.prev').onclick = function() {
+   // сдвиг влево
+   position += width * count;
+   // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+   position = Math.min(position, 0)
+   list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+   // сдвиг вправо
+   position -= width * count;
+   // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+   position = Math.max(position, -width * (listElems.length - count));
+   list.style.marginLeft = position + 'px';
+};
